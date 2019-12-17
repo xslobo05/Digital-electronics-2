ad77408a84b26da97144235774053dbb607446ed
lab661 <lab661@PC-447.urel.feec.vutbr.cz>
2019-12-17 11:59:05 +0100

    Finální verze

--- projects/PROJECT/main.c
+++ projects/PROJECT/main.c
@@ -3,11 +3,10 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "timer.h"
-#include "uart.h"
 #include "nokia5110.h"
 #include "usound.h"
 #include "twi.h"
-#include "hc05.h"
+#include "uart.h"
 #include <stdbool.h> 
 
 /* Typedef -----------------------------------------------------------*/
@@ -15,7 +14,7 @@
 /* Define ------------------------------------------------------------*/
 
 #define POCET_KOL 7+1
-#define UART_BAUD_RATE 38400
+#define UART_BAUD_RATE 9600
 /* Variables ---------------------------------------------------------*/
 
 /* Function prototypes -----------------------------------------------*/
@@ -84,15 +83,15 @@ void prujezd(uint8_t track, uint8_t round_max,uint8_t round, uint8_t count_min, 
         itoa(round,round_number,10);
         
         if(round > 3){
-            adjust_round = 4;
+            adjust_round = 4; //deleni dvema, pozor na pocet kol, ale flexibilnejsi
         }
 
         if(track == 1){
             if(round == 4){
-                nokia_lcd_set_cursor(0,10);nokia_lcd_write_string("        ",1);
-                nokia_lcd_set_cursor(0,20);nokia_lcd_write_string("        ",1);
-                nokia_lcd_set_cursor(0,30);nokia_lcd_write_string("        ",1);
-                nokia_lcd_set_cursor(0,40);nokia_lcd_write_string("        ",1);
+                nokia_lcd_set_cursor(0,10);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(0,20);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(0,30);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(0,40);nokia_lcd_write_string("      ",1);
                 nokia_lcd_set_cursor(0,(round + 1 - adjust_round)*10);
                 nokia_lcd_write_string(round_number,1); nokia_lcd_write_string(".",1);
                 nokia_lcd_write_string(disp_min,1); nokia_lcd_write_string(":",1); nokia_lcd_write_string(disp_sec,1);
@@ -105,10 +104,10 @@ void prujezd(uint8_t track, uint8_t round_max,uint8_t round, uint8_t count_min, 
 
         if(track == 2){
             if(round == 4){
-                nokia_lcd_set_cursor(48,10);nokia_lcd_write_string("         ",1);
-                nokia_lcd_set_cursor(48,20);nokia_lcd_write_string("         ",1);
-                nokia_lcd_set_cursor(48,30);nokia_lcd_write_string("         ",1);
-                nokia_lcd_set_cursor(48,40);nokia_lcd_write_string("         ",1);
+                nokia_lcd_set_cursor(48,10);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(48,20);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(48,30);nokia_lcd_write_string("      ",1);
+                nokia_lcd_set_cursor(48,40);nokia_lcd_write_string("      ",1);
                 nokia_lcd_set_cursor(48,(round + 1 - adjust_round)*10);
                 nokia_lcd_write_string(round_number,1); nokia_lcd_write_string(".",1);
                 nokia_lcd_write_string(disp_min,1); nokia_lcd_write_string(":",1); nokia_lcd_write_string(disp_sec,1);
@@ -132,7 +131,7 @@ void prujezd(uint8_t track, uint8_t round_max,uint8_t round, uint8_t count_min, 
 int main(void)
 {
     uint32_t distance, rtc;
-    char dist[5];
+    char dist[5], zobr_kol[3], zobr_min[3],zobr_sek[3];
     //char zobrazeni_sekund[3], zobrazeni_minut[3];
     bool detekce=1;
     uint8_t adresa_clocku = 0x68;
@@ -147,6 +146,7 @@ int main(void)
     //nokia_lcd_write_string("Vzdalenost[cm]",1);
     //nokia_lcd_render();
 
+    sei();
     /* Inicializace DS3231 casoveho obvodu */
     twi_init();
     
@@ -154,8 +154,9 @@ int main(void)
     usound_init();
 
     /* UART: asynchronous, 8-bit data, no parity, 1-bit stop */
-    //uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
-    //uart_puts("UART testing\r\n");
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+    //uart_puts("UART testing");
+    //uart_puts_p("UART testing");
 
     for(i=0;i<POCET_KOL;i++)
     {
@@ -166,7 +167,6 @@ int main(void)
     }
 
     for(;;){
-        
     /* Snímání vzdálenosti a zobrazení na displeji */
         _delay_ms(100);                         
         distance = readDistance();              
@@ -197,19 +197,21 @@ int main(void)
         if(distance <= 8){
             prujezd(1,POCET_KOL,kola1,pocitani_minut,pocitani_sekund,casy1);
             if (kola1 < POCET_KOL) kola1++;
-            _delay_ms(1000);
+            itoa(kola1-1, zobr_kol,10);
+            itoa(casy1[kola1-1][0], zobr_min,10);
+            itoa(casy1[kola1-1][1], zobr_sek,10);
+            uart_puts("\n\r\n\rDraha 1 prujezd, kolo: "); uart_puts(zobr_kol); uart_puts(", cas: "); uart_puts(zobr_min); uart_puts(":"); uart_puts(zobr_sek);
+            _delay_ms(500);
         }
-        if(distance >=9 && distance <= 16){
+        else if(distance >=9 && distance <= 16){
             prujezd(2,POCET_KOL,kola2,pocitani_minut,pocitani_sekund,casy2);
             if (kola2 < POCET_KOL) kola2++;
-            uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
-            uart_putc('x');
-            _delay_ms(1000);
+            itoa(kola2-1, zobr_kol,10);
+            itoa(casy2[kola2-1][0], zobr_min,10);
+            itoa(casy2[kola2-1][1], zobr_sek,10);
+            uart_puts("\n\r\n\rDraha 2 prujezd, kolo: "); uart_puts(zobr_kol); uart_puts(", cas: "); uart_puts(zobr_min); uart_puts(":"); uart_puts(zobr_sek);
+            _delay_ms(500);
         }
-        
-        
-    
-
         nokia_lcd_set_cursor(0,0);
         nokia_lcd_write_string("    ",1);
         nokia_lcd_set_cursor(0,0);
